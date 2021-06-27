0287024dec1d7c6f508013768eb668213451646c
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2021-06-28 01:03:05 +0600

    Update index.js

--- Lab_2_180042134/Tutorial/index.js
+++ Lab_2_180042134/Tutorial/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 7777;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+	res.send('<h1>Home Page</h1>');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running at http://localhost:${PORT}`);
 });
