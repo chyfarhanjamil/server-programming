9e7f3c4cad46f5e80546118b925a9b83cc4e4fc8
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2021-07-01 22:39:02 +0600

    Updated users.controllers.js

--- Lab_2_180042134/Fest2021/controllers/users.controller.js
+++ Lab_2_180042134/Fest2021/controllers/users.controller.js
@@ -1,38 +1,75 @@
-const getLogin = (req, res)=>{
-    res.render('users/login.ejs');
-};
-const postLogin = (req, res)=>{
-    const { email, password } = req.body;
-    console.log(email);
-    console.log(password);
-};
-const getRegister = (req, res)=>{
-    res.render('users/register.ejs', {errors: req.flash('errors')});
-};
-const postRegister = (req, res)=>{
-    const { name, email, password, confirm_password } = req.body;
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    console.log(confirm_password);
+const User = require('../models/User.model');
+const bcrypt = require('bcryptjs');
 
-    //data validation
-    const errors = []
-    if(!name || !email || !password || !confirm_password){
-        errors.push('All fields are required');
-    }
-    if(password.length<6){
-        errors.push('Password should be atleast 6 characters');
-    }
-    if(password != confirm_password){
-        errors.push('Passwords do not match');
-    }
-    if(errors.length > 0){
-       req.flash('errors', errors);
-        res.redirect('/users/register');
-    }else{
-        //create new user
-        res.redirect('/users/login');
-    }
+const getLogin = (req, res) => {
+	res.render('users/login.ejs');
 };
- module.exports = {getLogin, postLogin, getRegister, postRegister};
+const postLogin = (req, res) => {
+	const { email, password } = req.body;
+	console.log(email);
+	console.log(password);
+};
+const getRegister = (req, res) => {
+	res.render('users/register.ejs', { errors: req.flash('errors') });
+};
+const postRegister = (req, res) => {
+	const { name, email, password, confirm_password } = req.body;
+
+	//data validation
+	const errors = [];
+	if (!name || !email || !password || !confirm_password) {
+		errors.push('All fields are required');
+	}
+	if (password.length < 6) {
+		errors.push('Password should be atleast 6 characters');
+	}
+	if (password != confirm_password) {
+		errors.push('Passwords do not match');
+	}
+	if (errors.length > 0) {
+		req.flash('errors', errors);
+		res.redirect('/users/register');
+	} else {
+		//create new user
+		User.findOne({ email: email }).then((user) => {
+            if (user) {
+              errors.push("User already exists with this email!");
+              req.flash("errors", errors);
+              res.redirect("/users/register");
+            } else {
+              bcrypt.genSalt(10, (err, salt) => {
+                if (err) {
+                  errors.push(err);
+                  req.flash("errors", errors);
+                  res.redirect("/users/register");
+                } else {
+                  bcrypt.hash(password, salt, (err, hash) => {
+                    if (err) {
+                      errors.push(err);
+                      req.flash("errors", errors);
+                      res.redirect("/users/register");
+                    } else {
+                      const newUser = new User({
+                        name,
+                        email,
+                        password: hash,
+                      });
+                      newUser
+                        .save()
+                        .then(() => {
+                          res.redirect("/users/login");
+                        })
+                        .catch(() => {
+                          errors.push("Saving User to the daatabase failed!");
+                          req.flash("errors", errors);
+                          res.redirect("/users/register");
+                        });
+                    }
+                  });
+                }
+              });
+            }
+          });
+	}
+};
+module.exports = { getLogin, postLogin, getRegister, postRegister };
