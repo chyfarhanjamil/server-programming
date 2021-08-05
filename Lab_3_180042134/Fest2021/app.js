d89d79f599a52578263ed0196ec42cba3114fb1f
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2021-08-06 02:19:05 +0600

    worked on prog contest model

--- Lab_3_180042134/Fest2021/app.js
+++ Lab_3_180042134/Fest2021/app.js
@@ -1,55 +1,57 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const flash = require('connect-flash');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const session = require("express-session");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
 //passport strategy
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //Connection to db
 mongoose
-	.connect(process.env.MongoURI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log('Connected to Database!');
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+  .connect(process.env.MongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Database!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 //Static Resources
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //View Engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //session and flash
 app.use(
-	session({
-		secret: 'secret',
-		resave: true,
-		saveUninitialized: true,
-	})
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 //Body parser
 app.use(express.urlencoded({ extended: false }));
 
 //Routes
-const indexRoutes = require('./routes/index.routes');
-const userRoutes = require('./routes/users.routes');
-const MORoutes = require('./routes/mathOlympiad.routes');
+const indexRoutes = require("./routes/index.routes");
+const userRoutes = require("./routes/users.routes");
+const MORoutes = require("./routes/mathOlympiad.routes");
+const PCRoutes = require("./routes/progContest.routes");
+
 app.use(indexRoutes);
-app.use('/users', userRoutes);
-app.use('/MathOlympiad', MORoutes);
+app.use("/users", userRoutes);
+app.use("/MathOlympiad", MORoutes);
+app.use("/ProgContest", PCRoutes);
 
 module.exports = app;
