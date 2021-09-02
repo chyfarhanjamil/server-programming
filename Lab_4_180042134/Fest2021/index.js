12ca0f1d23073bf94538ca8798a5dc7bf91d79e1
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2021-09-02 20:46:13 +0600

    init

---
+++ Lab_4_180042134/Fest2021/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+//const request = require("./app");
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+    console.log(`App is running at http://localhost:${PORT}`);
+});
+
+
---
+++ Lab_4_180042134/Fest2021/routes/progContest.routes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  ensureAuthenticated,
+  addUserData,
+} = require("../middlewares/auth.middleware");
+
+const {
+  getPC,
+  postPC,
+  getPCList,
+  deletePC,
+  paymentDonePC,
+  selectPC,
+} = require("../controllers/progContest.controllers");
+
+router.get("/register", ensureAuthenticated, addUserData, getPC);
+router.post("/register", ensureAuthenticated, addUserData, postPC);
+router.get("/list", ensureAuthenticated, addUserData, getPCList);
+router.get("/pc-delete/:id", ensureAuthenticated, addUserData, deletePC);
+router.get(
+  "/pc-paymentDone/:id",
+  ensureAuthenticated,
+  addUserData,
+  paymentDonePC
+);
+router.get("/pc-select/:id", ensureAuthenticated, addUserData, selectPC);
+
+module.exports = router;
